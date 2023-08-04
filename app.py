b2dbe6aef9ba0f901291ded209f647f39d3c9e98
Brinzkii <dmbezehertny@gmail.com>
2023-08-04 14:18:30 -0400

    add templates and routes for home, login and signup

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, flash, redirect
 from models import connect_db
+from forms import SignupForm, LoginForm
 
 app = Flask(__name__)
 
@@ -12,3 +13,34 @@ app.config["SECRET_KEY"] = 'password'
 connect_db(app)
 
 #################################### Routes ####################################
+
+
+@app.route('/')
+def show_home():
+    """Show homepage"""
+
+    return render_template('home.html')
+
+
+@app.route('/signup')
+def signup_user():
+    """Signup a new user via class method, save hashed password to database"""
+
+    form = SignupForm()
+
+    if form.validate_on_submit():
+        print('success')
+    else:
+        return render_template('signup.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_user():
+    """Authenticate user via vlass method and login"""
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        print('success')
+    else:
+        return render_template('login.html', form=form)
---
+++ forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, TextAreaField
+from wtforms.validators import DataRequired, Email, Length
+
+
+class SignupForm(FlaskForm):
+    """Form for signing up new user"""
+
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password (6 char min)',
+                             validators=[Length(min=6)])
+    profile_img = StringField('Profile Image (optional)')
+
+
+class LoginForm(FlaskForm):
+    """Form for logging in existing user"""
+
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=6)])
